24fc595ba91c877be26e7c5d715c4655c70713cc
Kamal Nayan <geniusamansingh@gmail.com>
2023-03-25 13:08:24 +0000

    adding other chains

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,11 @@
+// @ts-nocheck
 import type { AppProps } from "next/app";
 import { ThirdwebProvider } from "@thirdweb-dev/react";
 import { Navbar } from "../components/Navbar/Navbar";
 import NextNProgress from "nextjs-progressbar";
 import { NETWORK } from "../consts/contractAddresses";
 import "../styles/globals.css";
+import { useEffect , useState  } from "react";
 // wagmi
 import { WagmiConfig, configureChains, createClient as createWagmiClient } from 'wagmi';
 import { publicProvider } from 'wagmi/providers/public';
@@ -23,8 +25,11 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 // import 'react-toastify/dist/ReactToastify.css';
 import { getWagmiChainConfig } from '../components/features/chains/metadata';
 import { Color } from '../styles/Color';
+import { ScrollAlphaTestnet } from "@thirdweb-dev/chains";
 // wagmi configs setup
 const { chains, provider } = configureChains(getWagmiChainConfig(), [publicProvider()]);
+
+
 
 const connectorConfig = {
   appName: 'X-chain Gaming NFT',
@@ -69,8 +74,43 @@ const reactQueryClient = new QueryClient({
 
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const [activeChain, setActiveChain] = useState<string>(NETWORK);
+
+  useEffect(() => {
+    if (window.ethereum) {
+      const ethereum = window.ethereum;
+      ethereum.request({ method: 'net_version' })
+        .then((networkId: string) => {
+          console.log(activeChain,networkId);
+          switch (networkId) {
+            case '1':
+              setActiveChain('mainnet');
+              break;
+            case '1287':
+              setActiveChain('moonbase-alpha');
+              break;
+            case '4':
+              setActiveChain('rinkeby');
+              break;
+            case '5':
+              setActiveChain('goerli');
+              break;
+            case '534353':
+              setActiveChain('ScrollAlphaTestnet');
+              break;
+            default:
+              setActiveChain(NETWORK);
+          }
+        })
+        .catch((error: any) => {
+          console.log('Error getting network ID:', error);
+          setActiveChain(NETWORK);
+        });
+    }
+  }, []);
+  console.log(activeChain);
   return (
-    <ThirdwebProvider activeChain={NETWORK}>
+    <ThirdwebProvider activeChain={ScrollAlphaTestnet}>
       {/* Progress bar when navigating between pages */}
       <NextNProgress
         color="var(--color-tertiary)"
